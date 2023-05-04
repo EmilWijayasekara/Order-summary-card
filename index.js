394ed5ee4a9724110ebe38ea31d9fbb6ec5f3db1
Emil <132345514+EmilWijayasekara@users.noreply.github.com>
2023-05-04 20:18:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function alartUser() {
+  alert("Thank you fro payment,We'll update your account shortly!");
+}
+
+function alartUserCancel() {
+  alert("Sorry, Payment is cancelled");
+}
+
+function alartUserChange() {
+  alert("Are you sure you want to change your plan?");
+}
